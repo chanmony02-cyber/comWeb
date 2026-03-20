1ca6f5c04d87cf780c8de7076208298d3a2da7d7
rothana <rothana@vhttechnology.com>
2026-03-20 20:16:09 +0700

    Update contact.js

--- src/data/contact/contact.js
+++ src/data/contact/contact.js
@@ -20,6 +20,6 @@ export const contactData = {
       { day: "Sunday:", hours: "Closed" },
     ],
     mapEmbedUrl:
-      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d62503.05241869578!2d104.89148434863281!3d11.556373699999994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3109519b00000001%3A0x1f3a9e36e4c5e6b1!2sPhnom%20Penh%2C%20Cambodia!5e0!3m2!1sen!2skh!4v1700000000000",
+      "https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3909.385303281338!2d104.96293999999999!3d11.524194!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTHCsDMxJzI3LjEiTiAxMDTCsDU3JzQ2LjYiRQ!5e0!3m2!1sen!2skh!4v1774012484878!5m2!1sen!2skh",
   },
 };
